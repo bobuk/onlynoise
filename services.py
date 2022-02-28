fb3b1ffe9a0c8b76cefa1c5f6f38bdf7cf6ac658
Grigory Bakunov <thebobuk@ya.ru>
2022-02-28 22:17:30 +0200

    move to new project structure and sse

---
+++ services.py
@@ -0,0 +1,23 @@
+class onlynoise:
+    image = "docker.rubedo.cloud/onlynoise:latest"
+    public = "onlynoise.rubedo.cloud @ 8080"
+    watchtower = True
+    envs = {"MONGO": "mongo"}
+
+class onlynoisepush:
+    image = "docker.rubedo.cloud/onlynoise-push:latest"
+    envs = "MONGO=mongo"
+    networks = ["public"]
+    doppler = True
+    watchtower = True
+
+class mongo:
+    image = "mongo:latest"
+    command = "mongod --quiet"
+    volumes = [
+        "./db:/data/db",
+        "./data/docker-entrypoint-initdb.d:/docker-entrypoint-initdb.d"]
+    class envs:
+        MONGO_INITDB_DATABASE="ondb"
+    networks = ["public"]
+    watchtower = True
